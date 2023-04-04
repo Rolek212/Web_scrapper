18d01d1fea23d2af05651838642551278e4bd140
Rolek212 <rolek212@gmail.com>
2023-04-04 23:41:03 +0200

    scraping and saving to csv file

--- scrapper.py
+++ scrapper.py
@@ -1,19 +1,64 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
-
+#main site
 html_text = requests.get('http://www.ilekarze.pl/forum/Fora-tematyczne/Zdrowie-kobiety/').text
-f = open("html.txt", "w")
-f.write(html_text)
-f.close()
+
+#searching for posts
 soup = BeautifulSoup(html_text, 'lxml')
 topics = soup.find_all('td', 'td_post_tytul')
-f = open("test.txt", "w")
+
+#file
+f = open("test.csv", "w")
+writer = csv.writer(f)
+writer.writerow(["Date", "Author", "Topic", "Content"])
+
 for topic in topics:
     link = topic.find('a')
-    titel = topic.find('a', 'a_text')
-    print(titel.string)
-    print(link.get('href'))
-    f.write(titel.string + ' ')
-    f.write(link.get('href') + '\n')
+    
+    #going to single post
+    site = requests.get(link.get('href')).text
+    site = BeautifulSoup(site, 'lxml')
+    
+    #post
+    post = site.find('div', 'forum_post')
+    
+    #date of the post
+    date = post.find_all('p')
+    date = date[-1].text
+    
+    #author of the post
+    author = post.find('p')
+    author = author.find('a').text
+    
+    #title of the post
+    title = post.find('h2').text
+    
+    #content of the post
+    post_content = site.find('div', 'tresc_posta').text
+    
+    #removing unnecessary content
+    post_content = post_content.replace('Udostępnij', '')
+    post_content = post_content.replace(' » Link do posta » Zgłoś do moderacji', '')
+    post_content = post_content.strip()
+
+    #writing to file
+    writer.writerow([date, author, title, post_content])
+
 f.close()
+
+#testing space(to be deleted)
+'''
+#site = requests.get('http://www.ilekarze.pl/forum/Fora-tematyczne/Zdrowie-kobiety/mleko-modyfikowane-1666903947.html').text
+site = requests.get('http://www.ilekarze.pl/forum/Fora-tematyczne/Zdrowie-kobiety/objawy-menopauzy.html').text
+soup2 = BeautifulSoup(site, 'lxml')
+post = soup2.find('div', 'forum_post')
+date = post.find('h2').text
+#date = date.find('a').text
+f = open('oi.txt', 'w')
+#f.write(str(post))
+#f.write(author + ' ')
+f.write(date)
+f.close()
+'''
